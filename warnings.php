9978b7c638979095b57852c793142edffee76348
Mary Murphy <marymurphy@g.harvard.edu>
2014-09-28 19:20:03 -0400

    tweaked comments

--- warnings.php
+++ warnings.php
@@ -7,7 +7,7 @@ if (!array_key_exists ("submit", $_GET)) {
 
 else {
 
-	//If the user has not selected at least one word list, they are reminded to do so
+	//if the user has not selected at least one word list, they are reminded to do so
 	if (!array_key_exists ("james", $_GET) 
 		AND !array_key_exists ("shakespeare", $_GET) 
 		AND !array_key_exists ("rude", $_GET) 
@@ -15,7 +15,7 @@ else {
 			$passPhrase = "Please select at least one word list!";
 	}
 
-	//If the user does not specify a number of words, they are reminded to do so
+	//if the user does not specify a number of words, they are reminded to do so
 	if ($_GET["count"] <= 0 || $_GET["count"] > 7) {
 			$passPhrase = "Please specify a number of words between 1-7!";
 	}
