3b27db8c828a5b0fd1e3165b1f8dbee753a532e9
Mary Murphy <marymurphy@g.harvard.edu>
2014-09-28 19:19:28 -0400

    added comments to explain process

--- warnings.php
+++ warnings.php
@@ -1,15 +1,13 @@
 <?php
 
+//if no data has been submitted as yet, default the message to indicate where the password will appear
 if (!array_key_exists ("submit", $_GET)) {
 	$passPhrase = "will appear here";
 }
 
 else {
 
-	reset($_GET);
-
 	//If the user has not selected at least one word list, they are reminded to do so
-
 	if (!array_key_exists ("james", $_GET) 
 		AND !array_key_exists ("shakespeare", $_GET) 
 		AND !array_key_exists ("rude", $_GET) 
@@ -18,7 +16,6 @@ else {
 	}
 
 	//If the user does not specify a number of words, they are reminded to do so
-
 	if ($_GET["count"] <= 0 || $_GET["count"] > 7) {
 			$passPhrase = "Please specify a number of words between 1-7!";
 	}
