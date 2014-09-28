9dab113caa9e87d1299789dd73b26b9bd5a34223
Mary Murphy <marymurphy@g.harvard.edu>
2014-09-28 19:22:54 -0400

    organized and made bootstrap live

--- head.php
+++ head.php
@@ -1,10 +1,12 @@
 <head>
 
 	<title>King James Shakespeare's Seemingly Rude and Nonsensical Password Generator</title>
+
 	<meta charset="UTF-8">
-	<meta name="description" content="Describe website here">
-	<meta name="keywords" content="Put keywords separated by commas here">
+	<meta name="description" content="password generator">
+	<meta name="keywords" content="password, passwords, generator, generation, generating, Shakespeare, King James Bible, nonsense, rude">
+
+	<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">
 	<link rel="stylesheet" href="stylesheet.css" type="text/css">
-	<!--<link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.2.0/css/bootstrap.min.css">-->
 	
 </head>
